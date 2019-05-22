c138d7a3fa7eafc3f9ba93e82b0815f573d75720
Noon-R <r1234ppsp@yahoo.co.jp>
2019-05-22 13:49:46 +0900

    Make ForceCulculater

--- PhysicsUtil/PhysicsUtil.cs
+++ PhysicsUtil/PhysicsUtil.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace PhysicsUtil{
-    public static class Force{
+     public class ForceCulculater{
         
-        public static Vector3 GetNeededForceToTerminalVelocity( Vector3 targetVelocity,float mass, float drug ){
-
+        private float mMass = 0;
+        private float mDrag = 0;
+        public ForceCulculater(float mass, float drag){
+            mMass = mass;
+            mDrag = drag;
 
         }
+        public Vector3 GetNeededForceToTerminalVelocity(Vector3 targetVelocity){
+            return new Vector3(
+                GetNeededForceToTerminalSpeed(targetVelocity.x),
+                GetNeededForceToTerminalSpeed(targetVelocity.y),
+                GetNeededForceToTerminalSpeed(targetVelocity.z)
+            );
+        }
 
-        public static float GetNeededForceToTerminalSpeed(float targetSpeed, float mass , float drug){
-            return targetSpeed * mass * drug / 1 - drug *  Time
+        public float GetNeededForceToTerminalSpeed(float targetSpeed){
+            return (targetSpeed * mMass * mDrag )/( 1 - mDrag * Time.fixedDeltaTime);
         }
     }
 }
